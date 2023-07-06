6120d3feadc9af63ff9972f690d72639de8c51f1
Oleksandrov1995 <olexandrov1995@gmail.com>
2023-07-06 21:03:59 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import { Section } from './Section/Section.jsx';
 import { Statistics } from './Statistics/Statistics.jsx';
@@ -8,45 +9,46 @@ export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  
+  
+    const handleLeaveFeedback = option => {
+      switch (option) {
+        case 'good':
+          setGood(prevGood => prevGood + 1);
+          break;
+        case 'neutral':
+          setNeutral(prevNeutral => prevNeutral + 1);
+          break;
+        case 'bad':
+          setBad(prevBad => prevBad + 1);
+          break;
+        default:
+          return;
+      }
+    };
 
   const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
   const countPositiveFeedbackPercentage = () => {
-    return Math.round((good / countTotalFeedback()) * 100);
+    return Math.round((good / countTotalFeedback()) * 100) || 0;
   };
 
   const total = countTotalFeedback();
   
   const positivePercentage = countPositiveFeedbackPercentage();
 
-  const handleLeaveFeedback = e => {
-    switch (e) {
-      case 'good':
-        setGood(prevGood => prevGood + 1);
-        break;
-      case 'neutral':
-        setNeutral(prevNeutral => prevNeutral + 1);
-        break;
-      case 'bad':
-        setBad(prevBad => prevBad + 1);
-        break;
-      default:
-        return;
-    }
-  };
+ 
 
   return (
     <>
       <Section title="Please leave feadback">
         <FeedbackOptions
-          options={{ good, neutral, bad }}
+        options={['good', 'neutral', 'bad']}
           onLeaveFeedback={handleLeaveFeedback}
         />
-        {total === 0 ? (
-          <Notification message="There is no feedback" />
-        ) : (
+        {total ? (
           <Statistics
             good={good}
             neutral={neutral}
@@ -54,6 +56,8 @@ export const App = () => {
             total={total}
             positivePercentage={positivePercentage}
           />
+        ): (
+          <Notification message="There is no feedback" />
         )}
       </Section>
     </>
